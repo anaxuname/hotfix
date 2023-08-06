9c28c0ca2d96d78e4c6e69e028718c7bf8fe3937
Anaxuname <anaxuname@gmail.com>
2023-08-06 20:38:16 +0300

    fixed tests

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -2,7 +2,11 @@ from utils import arrs
 
 
 def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 3
+    assert arrs.get([1, 2, 3], 1, "test") == 2
+    assert arrs.get([1, 2, 3], -1, "test") == "test"
+    assert arrs.get([1, 2, 3], 2, "test") == 3
+    assert arrs.get([1, 2, 3], 3, "test3") == "test3"
+    assert arrs.get([1, 2, 3], 100) is None
     assert arrs.get([], 0, "test") == "test"
 
 
